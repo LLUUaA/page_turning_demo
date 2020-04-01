f339bf1ccfb7d8fb8f6dbb889fc6f9cc3de71081
lluuaa <1012758795@qq.com>
2020-04-01 18:46:40 +0800

    - 拆分

--- lib/pageTurning.dart
+++ lib/pageTurning.dart
@@ -1,47 +1,10 @@
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 import './core/reader/reader.dart';
 
-class PageTurningDemo1 extends StatefulWidget {
-  @override
-  _PageTurningDemo1 createState() => _PageTurningDemo1();
-}
-
-class _PageTurningDemo1 extends State<PageTurningDemo1> {
-  _PageTurningDemo1() {
-    /// SystemChrome
-    SystemChrome.setEnabledSystemUIOverlays([]);
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.black,
-      systemNavigationBarColor: Colors.black,
-      systemNavigationBarDividerColor: Colors.black,
-    ));
-  }
-
-  Offset offset = Offset.zero; // move offset
-
+class PageTurningDemo1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onPanDown: (DragDownDetails _) {
-        MyReader.handlePanDown(_, size);
-      },
-      onPanUpdate: (DragUpdateDetails _) {
-        setState(() {
-          offset = _.globalPosition;
-        });
-      },
-      onPanEnd: (DragEndDetails _) {
-        setState(() {
-          offset = Offset.zero;
-        });
-      },
-      child: CustomPaint(
-        size: size,
-        painter: MyReader(offset: offset),
-      ),
-    );
+    return MyReader();
   }
 }
